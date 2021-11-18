114a05cf5c0cdf4bc8d572101b84fd4a115eccbd
Rituraj Mahato <94545894+Rituraj-M50@users.noreply.github.com>
2021-11-18 09:18:48 +0530

    Create Addition of Tuples.py

---
+++ Tuple Manipulation/Addition of Tuples.py
@@ -0,0 +1,9 @@
+tuple1 = (10, 16, 8) 
+tuple2 = (3, 5, 18) 
+ 
+print("The original tuple 1 : " + str(tuple1)) 
+print("The original tuple 2 : " + str(tuple2)) 
+
+result = tuple(map(lambda i, j: i + j, tuple1, tuple2)) 
+
+print("Resultant Tuple after Addition : " + str(result)) 
